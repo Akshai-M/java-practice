42bb46ca728359832f25dcc189f1aec8e61fc337
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-03-23 14:18:07 +0530

    commit

--- simple.java
+++ simple.java
@@ -1,6 +1,29 @@
-package
-public class main{
-    public static void main(string args[]){
+import java.util.Scanner;
+
+// public class simple {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Enter your name:");
+//         String name = scanner.nextLine();
+//         System.out.println("Hello, " + name + "!");
+//         scanner.close();
+//     }
+// }
+public class simple {
+    public static void main(String[] args){
+        int resp = sum();
+        System.out.print("Result:"+resp);
+
+    }
+    static int sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a = sc.nextInt();
+        System.out.print("Enter b:");
+        int b = sc.nextInt();
+        int res = a + b;
+        return res;
+        // System.out.print("Result:"+res);
 
     }
 }
