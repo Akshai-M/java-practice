57bde87a291d20f1e2b1b47054d83f1e0f51515f
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-03-27 04:55:12 +0530

    modified

--- arr.java
+++ arr.java
@@ -6,16 +6,15 @@ import java.util.Arrays;
 //         change(arr);
 //         // System.out.print(Arrays.toString(arr));
 //         fun(2,5,95,4,4,5,5,5,4,68,4,8,84);
-        
+
 //     }
 
 //     static void change(int[] nums) {
 //         nums[0] = 99;
 //     }
 //     static void fun(int a, int b,int...v){
-        
-//     System.out.print(a + " " + b + " " + Arrays.toString(v));
 
+//     System.out.print(a + " " + b + " " + Arrays.toString(v));
 
 //     }
 // }
@@ -24,9 +23,9 @@ public class arr {
     public static void main(String[] args) {
         // Create and initialize a 3D array
         int[][][] array3D = {
-            { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} },
-            { {10, 11, 12}, {13, 14, 15}, {16, 17, 18} },
-            { {20, 21, 22}, {23, 24, 25}, {26, 27, 28} }
+                { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } },
+                { { 10, 11, 12 }, { 13, 14, 15 }, { 16, 17, 18 } },
+                { { 20, 21, 22 }, { 23, 24, 25 }, { 26, 27, 28 } }
         };
 
         // Printing the 3D array
